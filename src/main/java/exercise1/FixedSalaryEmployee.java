24ec10e30837ba4d57a4adb585bfaa6b0a83c69f
Zyvrox <mehdi.zarioh@gmail.com>
2018-11-09 14:31:04 +0100

    Premier commit sur l'exo 1

---
+++ src/main/java/exercise1/FixedSalaryEmployee.java
@@ -0,0 +1,14 @@
+package exercise1;
+
+public class FixedSalaryEmployee extends Employee{
+	public FixedSalaryEmployee(String a, int b)
+	{
+		name=a;
+		salary=b;
+	}
+
+	public int FixedSalaryEmployee()
+	{
+		return salary;
+	}
+}
